b0cf190627d98bfc909085ed424a90f1dc5a521a
exc414mac <carlos@bluephoenix.io>
2017-06-25 01:56:18 -0400

    UI Finished
    First commit. UI finished.

---
+++ RetroCalculator/RetroCalculatorVC.swift
@@ -0,0 +1,20 @@
+//File name    : RetroCalculatorVC.swift
+//Project Name : RetroCalculator
+//Created by   : Carlos Perez
+//Create on    : 6/24/17
+
+import UIKit
+
+class RetroCalculatorVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad();
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning();
+    }
+}
+
